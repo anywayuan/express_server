57bcf87542e9a6ec193c8c200928b875ba3b0f39
anyway <59185988+Yoankiss@users.noreply.github.com>
2022-01-24 14:13:08 +0800

    Update db.js

--- conf/db.js
+++ conf/db.js
@@ -1,10 +1,10 @@
 // const env = process.env.NODE_ENV
 let MYSQL_CONF = {
-  host: 'rm-bp1505rz1nrdyf2v21o.mysql.rds.aliyuncs.com',
-  user: 'blog_database',
-  password: 'KY-9ndJnL4&HVcu',
+  host: '',
+  user: '',
+  password: '',
   port: '3306',
-  database: 'myblog'
+  database: ''
 }
 let REDIS_CONF = {
   port: 6379,
